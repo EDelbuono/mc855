84b8cd5d74956b55c10776fb3dc488beb45a3c5b
andre luis <andreprog@hotmail.com>
2022-07-14 15:56:09 -0300

    Update ChatFlow.js
    added name and numero_hc checkings, fixed loop (8.1.1 -> 9.1.1.4 -> 8.1.1 -> 9.1.1.4) and now displaying specialities available to the date/time of the app's access

--- src/chatbot/ChatFlow.js
+++ src/chatbot/ChatFlow.js
@@ -1,3 +1,6 @@
+var nome='nome_teste';
+var numero_hc;
+
 export const steps=[
     {
         id: '1',
@@ -13,11 +16,24 @@ export const steps=[
         id: '3',
         user: true,
         trigger: '4',
+        validator: value => {
+            let pattern = /^[-a-zA-Z'áàâãéèêíïóôõöúçñ ]+$/;
+            if(pattern.test(value)){
+                let nome = value;
+                //alert(nome);
+                return true;
+            }
+            else
+                return 'Nome inválido. Números não são aceitos.'
+        }
     },
     {
         id: '4',
         message: 'Oi, {previousValue}! É um prazer ter você com a gente! Agora me diz: você já é cliente do HC?',
-        trigger: '5',
+        trigger: v => {
+            //alert(nome);
+            return '5'
+        },
     },
     {
         id: '5',
@@ -83,7 +99,7 @@ export const steps=[
         { value: 1, label: 'Sim', trigger: '9.1.1.1'},
         { value: 2, label: 'Não, mas recebi o e-mail de encaixe', trigger: '9.1.1.2'},
         { value: 3, label: 'Não, mas gostaria/preciso passar pelo atendimento hoje', trigger: '9.1.1.3'},
-        { value: 4, label: 'Respondi errado. Voltar para a tela anterior', trigger: '9.1.1.4'},
+        { value: 4, label: 'Respondi errado. Voltar para a tela anterior', trigger: '5.1'},
         ],
     },
     {
@@ -95,11 +111,92 @@ export const steps=[
         id: '8.1.3',
         user: true,
         trigger: '9.1.3',
+        validator: value => {
+            let pattern = /^[0-9 ]+$/;
+            if(pattern.test(value)){
+                numero_hc = value;
+                return true;
+            }
+            else
+                return 'Só são aceitos dígitos.'
+        }
     },
     {
         id: '9.1.1.1',
-        message: 'Qual a especialidade do seu atendimento?',
-        trigger: '10.1.1.1',
+        message: resp => {
+            const d = new Date();
+            let hour = d.getHours();
+            let day = d.getDay();
+            
+            const msg = ' Qual a especialidade do seu atendimento?';
+            if(day===1){
+                if(hour<11 && hour>18)
+                    return 'Exibindo especialidades disponíveis de Segunda de Manhã.' + msg;
+                else
+                    return 'Exibindo especialidades disponíveis de Segunda de Tarde.' + msg;
+            } else if(day===2){
+                if(hour<11 && hour>18)
+                    return 'Exibindo especialidades disponíveis de Terça de Manhã.' + msg;
+                else
+                    return 'Exibindo especialidades disponíveis de Terça de Tarde.' + msg;
+            } else if(day===3){
+                if(hour<11 && hour>18)
+                    return 'Exibindo especialidades disponíveis de Quarta de Manhã.' + msg;
+                else
+                    return 'Exibindo especialidades disponíveis de Quarta de Tarde.' + msg;
+            } else if(day===4){
+                if(hour<11 && hour>18)
+                    return 'Exibindo especialidades disponíveis de Quinta de Manhã.' + msg;
+                else
+                    return 'Exibindo especialidades disponíveis de Quinta de Tarde.' + msg;
+            } else if(day===5){
+                if(hour<11 && hour>18)
+                    return 'Exibindo especialidades disponíveis de Sexta de Manhã.' + msg;
+                else
+                    return 'Exibindo especialidades disponíveis de Sexta de Tarde.' + msg;
+            } 
+
+            return 'Exibindo especialidades disponíveis de Segunda de Manhã.' + msg;
+        },
+        trigger: id_resp => {
+            const d = new Date();
+            let hour = d.getHours();
+            let day = d.getDay();
+            
+            if(day===1){
+                if(hour<11)
+                    return '10.1.1.1-seg-man'
+                else if(hour<18 )
+                    return '10.1.1.1-seg-tar'
+                else 
+                    return '10.1.1.1-ter-man'
+            } else if(day===2){
+                if(hour<11 && hour>18)
+                    return '10.1.1.1-ter-man'
+                else if(hour<18 )
+                    return '10.1.1.1-ter-tar'
+                else return '10.1.1.1-qua-man'
+            } else if(day===3){
+                if(hour<11 && hour>18)
+                    return '10.1.1.1-qua-man'
+                else if(hour<18 )
+                    return '10.1.1.1-qua-tar'
+                else return '10.1.1.1-qui-man'
+            } else if(day===4){
+                if(hour<11 && hour>18)
+                    return '10.1.1.1-qui-man'
+                else if(hour<18 )
+                    return '10.1.1.1-qui-tar'
+                else return '10.1.1.1-sex-man'
+            } else if(day===5){
+                if(hour<11 && hour>18)
+                    return '10.1.1.1-sex-man'
+                else if(hour<18 )
+                    return '10.1.1.1-sex-tar'
+            } 
+
+            return '10.1.1.1'
+        },
     },
     {
         id: '9.1.1.2',
@@ -130,6 +227,30 @@ export const steps=[
     },
     {
         id: '10.1.1.1',
+        message: 'Infelizmente não temos horário de atendimento de fim de semana. Nós trabalhamos de segunda a quinta das 7h - 17h.',
+        end: true,
+    },
+    // {
+    //     id: '10.1.1.1-opcoes',
+    //     message: 'Selecione qual o período disponível para você:',
+    //     options: opt => {
+    //         disponivel = false;
+    //         return [
+    //         { value: 1, label: 'Segunda-Feira de Manhã', trigger: '10.1.1.1-seg-man'},
+    //         { value: 2, label: 'Segunda-Feira de Tarde', trigger: '10.1.1.1-seg-tar'},
+    //         { value: 3, label: 'Terça-Feira de Manhã', trigger: '10.1.1.1-ter-man'},
+    //         { value: 4, label: 'Terça-Feira de Tarde', trigger: '10.1.1.1-ter-tar'},
+    //         { value: 5, label: 'Quarta-Feira de Manhã', trigger: '10.1.1.1-qua-man'},
+    //         { value: 6, label: 'Quarta-Feira de Tarde', trigger: '10.1.1.1-qua-tar'},
+    //         { value: 7, label: 'Quinta-Feira de Manhã', trigger: '10.1.1.1-qui-man'},
+    //         { value: 8, label: 'Quinta-Feira de Tarde', trigger: '10.1.1.1-qui-tar'},
+    //         { value: 9, label: 'Sexta-Feira de Manhã', trigger: '10.1.1.1-sex-man'},
+    //         { value: 10, label: 'Sexta-Feira de Tarde', trigger: '10.1.1.1-sex-tar'},
+    //         { value: 11, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+    //         ]},
+    // },
+    {
+        id: '10.1.1.1-seg-man',
         options: [
             { value: 1, label: 'Alergia e Imunologia Ped', trigger: '11.1.1.1'},
             { value: 2, label: 'Endocrinologia Ped', trigger: '11.1.1.1'},
@@ -137,6 +258,116 @@ export const steps=[
             { value: 4, label: 'Reumatologia Ped', trigger: '11.1.1.1'},
             { value: 5, label: 'Outro', trigger: '11.1.1.2'},
             { value: 6, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-seg-tar',
+        options: [
+            { value: 1, label: 'Medicina do Adolescente', trigger: '11.1.1.1'},
+            { value: 2, label: 'Violência Contra Criança e Adolescente', trigger: '11.1.1.1'},
+            { value: 3, label: 'Hipotiroidismo Cong I', trigger: '11.1.1.1'},
+            { value: 4, label: 'Obesidade', trigger: '11.1.1.1'},
+            { value: 5, label: 'Pneumologia PED', trigger: '11.1.1.1'},
+            { value: 6, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 7, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-ter-man',
+        options: [
+            { value: 1, label: 'Alergia e Imunologia Ped ', trigger: '11.1.1.1'},
+            { value: 2, label: 'Crescimento', trigger: '11.1.1.1'},
+            { value: 3, label: 'Gastropediatria', trigger: '11.1.1.1'},
+            { value: 4, label: 'Pneumologia Ped', trigger: '11.1.1.1'},
+            { value: 5, label: 'Reumatologia', trigger: '11.1.1.1'},
+            { value: 6, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 7, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-ter-tar',
+        options: [
+            { value: 1, label: 'Alergia e Imunologia Ped', trigger: '11.1.1.1'},
+            { value: 2, label: 'Crescimento', trigger: '11.1.1.1'},
+            { value: 3, label: 'Mucoviscidose', trigger: '11.1.1.1'},
+            { value: 4, label: 'Infectologia Pediátrica', trigger: '11.1.1.1'},
+            { value: 5, label: 'Cirurgia Pediátrica', trigger: '11.1.1.1'},
+            { value: 6, label: 'Genética', trigger: '11.1.1.1'},
+            { value: 7, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 8, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-qua-man',
+        options: [
+            { value: 1, label: 'Cardiologia Ped', trigger: '11.1.1.1'},
+            { value: 2, label: 'Triagem Neonatal', trigger: '11.1.1.1'},
+            { value: 3, label: 'Hipotiroidismo Cong I', trigger: '11.1.1.1'},
+            { value: 4, label: 'Reumatologia', trigger: '11.1.1.1'},
+            { value: 5, label: 'Genética', trigger: '11.1.1.1'},
+            { value: 6, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 7, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-qua-tar',
+        options: [
+            { value: 1, label: 'Alergia e Imunologia Ped', trigger: '11.1.1.1'},
+            { value: 2, label: 'Cardiologia Ped', trigger: '11.1.1.1'},
+            { value: 3, label: 'Diabetes FC', trigger: '11.1.1.1'},
+            { value: 4, label: 'Triagem Neonatal', trigger: '11.1.1.1'},
+            { value: 5, label: 'Obesidade', trigger: '11.1.1.1'},
+            { value: 6, label: 'Reumatologia', trigger: '11.1.1.1'},
+            { value: 7, label: 'Genética', trigger: '11.1.1.1'},
+            { value: 8, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 9, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+              
+        id: '10.1.1.1-qui-man',
+        options: [
+            { value: 1, label: 'Alergia e Imunologia Ped', trigger: '11.1.1.1'},
+            { value: 2, label: 'Endocrinologia Ped', trigger: '11.1.1.1'},
+            { value: 3, label: 'Gastropediatria', trigger: '11.1.1.1'},
+            { value: 4, label: 'Reumatologia', trigger: '11.1.1.1'},
+            { value: 5, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 6, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-qui-tar',
+        options: [
+            { value: 1, label: 'Ambulatório de Entrada/ Pediatria Integração', trigger: '11.1.1.1'},
+            { value: 2, label: 'Endocrinologia Ped', trigger: '11.1.1.1'},
+            { value: 3, label: 'Reumatologia', trigger: '11.1.1.1'},
+            { value: 4, label: 'Genética', trigger: '11.1.1.1'},
+            { value: 5, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 6, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-sex-man',
+        options: [
+                 
+            { value: 1, label: 'Alergia e Imunologia Ped', trigger: '11.1.1.1'},
+            { value: 2, label: 'Endocrinologia Ped', trigger: '11.1.1.1'},
+            { value: 3, label: 'Pediatria Integração', trigger: '11.1.1.1'},
+            { value: 4, label: 'Pneumologia PED', trigger: '11.1.1.1'},
+            { value: 5, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 6, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
+            ],
+    },
+    {
+        id: '10.1.1.1-sex-tar',
+        options: [
+            { value: 1, label: 'Medicina do Adolescente', trigger: '11.1.1.1'},
+            { value: 2, label: 'Cardiologia Ped', trigger: '11.1.1.1'},
+            { value: 3, label: 'Endocrinologia Ped', trigger: '11.1.1.1'},
+            { value: 4, label: 'Gastroent erologia Ped', trigger: '11.1.1.1'},
+            { value: 5, label: 'Triagem Mucoviscidose', trigger: '11.1.1.1'},
+            { value: 6, label: 'Outro', trigger: '11.1.1.2'},
+            { value: 7, label: 'Respondi errado. Voltar para a tela anterior.', trigger: '11.1.1.3'},
             ],
     },
     {
@@ -166,7 +397,22 @@ export const steps=[
     },
     {
         id: '11.1.1.2',
-        message: 'Nesse caso, a especialidade que você busca não tem profissionais atendendo nesse período. Para ver a lista de especialidades atendidas em cada dia e período, acesse <link>',
+        message: 'Nesse caso, a especialidade que você busca não tem profissionais atendendo nesse período. Abaixo segue a lista de especialidades em cada período:',
+        trigger: '11.1.1.2.1',
+    },
+    {
+        id: '11.1.1.2.1',
+        // eslint-disable-next-line no-multi-str
+        message: "Segunda-Feira de Manhã: Alergia e Imunologia Ped Endocrinologia Ped  Gastroenterologia Ped/Hepatologia I (Gastrocentro )  Reumatologia Ped \
+        Segunda-Feira de Tarde: Medicina do Adolescente Violência Contra Criança e Adolescente Hipotireoidismo Cong I Obesidade  Pneumologia PED \
+        Terça-Feira de Manhã: Alergia e Imunologia Ped Crescimento Gastropediatria  Pneumologia Ped  Reumatologia  \n\
+        Terça-Feira de Tarde: Alergia e Imunologia Ped Crescimento  Mucoviscidose  Infectologia Pediátrica  Cirurgia Pediátrica  Genética  \
+        Quarta-Feira de Manhã: Cardiologia Ped Triagem Neonatal Reumatologia Genética  \n\
+        Quarta-Feira de Tarde: Alergia e Imunologia Ped Cardiologia Ped Diabetes FC  Triagem Neonatal  Obesidade  Reumatologia  Genética  \
+        Quinta-Feira de Manhã: Alergia e Imunologia Ped Endocrinologia Ped  Gastropediatria  Reumatologia \
+        Quinta-Feira de Tarde: Ambulatório de Entrada/ Pediatria Integração Endocrinologia Ped  Reumatologia  Genética \
+        Sexta-Feira de Manhã: Alergia e Imunologia Ped Endocrinologia Ped  Pediatria Integração Pneumologia  \
+        Sexta-Feira de Tarde: Medicina do Adolescente Cardiologia Ped Endocrinologia Ped Gastroenterologia Ped Triagem Mucoviscidose",
         trigger: '12.1.1.2',
     },
     {
@@ -199,6 +445,15 @@ export const steps=[
         id: '12.1.1.1',
         user: true,
         trigger: '13.1.1.1',
+        validator: value => {
+            let pattern = /^[0-9 ]+$/;
+            if(pattern.test(value)){
+                numero_hc = value;
+                return true;
+            }
+            else
+                return 'Só são aceitos dígitos.'
+        }
     },
     {
         id: '12.1.1.2',
@@ -209,7 +464,7 @@ export const steps=[
     },
     {
         id: '13.1.1.1',
-        message: 'Você já está na fila e sua senha é <XX>. Sua senha e a sala onde acontecerá sua consulta aparecerão na Tela da TV acima da porta do ambulatório quando chegar sua vez.',
+        message: nome + ' HC: ' + numero_hc + ', você já está na fila e sua senha é <XX>. Sua senha e a sala onde acontecerá sua consulta aparecerão na Tela da TV acima da porta do ambulatório quando chegar sua vez.',
         trigger: '14.1.1.1',
     },
     {
@@ -231,7 +486,7 @@ export const steps=[
     },
     {
         id: '17.1.1.1',
-        message: 'Sem problemas, vamos de novo! Qual a especialidade do seu atendimento?',
-        trigger: '10.1.1.1',
+        message: 'Sem problemas, vamos de novo!',
+        trigger: '9.1.1.1',
     },
     ]
